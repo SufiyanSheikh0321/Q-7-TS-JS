c7c96afbbd23f7c41a98ab626a919d84a66adc1e
Muhammed Sufiyan <148213204+SufiyanSheikh0321@users.noreply.github.com>
2024-02-29 19:09:16 +0500

    Add files via upload

---
+++ Q.7.js
@@ -0,0 +1,18 @@
+"use strict";
+// 7. Number Eight: Write addition, subtraction, multiplication, and division operation that each result in the number 8. Be sure to enclose your operations in print statements to see the result.
+// 1.Addition
+let a = 5;
+let b = 3;
+//2.Subtraction
+let c = 11;
+let d = 3;
+//3.Multiplication
+let e = 2;
+let f = 4;
+//4.Division
+let g = 16;
+let h = 2;
+console.log(a + b);
+console.log(c - d);
+console.log(e * f);
+console.log(g / h);
---
+++ Q.7.ts
@@ -0,0 +1,23 @@
+// 7. Number Eight: Write addition, subtraction, multiplication, and division operation that each result in the number 8. Be sure to enclose your operations in print statements to see the result.
+
+
+let a: number = 5
+let b: number = 3
+
+
+// 1. Addition
+console.log(a + b);
+
+// 2. Subtraction
+console.log(a - b);
+
+// 3.Multiplication
+console.log(a * b);
+
+// 4.Division 
+console.log(a / b);
+
+
+
+
+
